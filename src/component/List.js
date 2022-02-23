49fe9bc72d3a26de783c860833d00964e8d1aee6
warrenwalt <warrenwalt19@gmail.com>
2022-02-23 11:54:11 +0300

    is complete feature

--- src/component/List.js
+++ src/component/List.js
@@ -3,14 +3,33 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 export default function List({list, deleteList}){
+    const [checked, setChecked] = React.useState(false)
+
+    function handleChange(e){
+        setChecked(e.target.checked)
+        console.log(e.target.checked)
+    }
+
+    const style = checked ? {textDecoration: "line-through"}: {textDecoration: "inherit"}
+
     return (
         <div>
-            <pre>{list}</pre>
+        <form onSubmit={e => e.preventDefault()}>
+            <input 
+            style={{color: "red"}}
+                type="checkbox"
+                name='isComplete'
+                onChange={handleChange}
+                checked={checked}
+                id="isComplete"
+            />
+            <pre style={style}>{list}</pre>
             <button
                 onClick={() => deleteList(list)}
             >
             Delete
             </button>
+        </form>
         </div>
     )
 }
