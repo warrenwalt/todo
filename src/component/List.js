e1187d3de3d987be2f78e9b4757ca6317fc088d4
warrenwalt <warrenwalt19@gmail.com>
2022-02-25 15:03:47 +0300

    minor fixes

--- src/component/List.js
+++ src/component/List.js
@@ -16,48 +16,49 @@ export default function List({list, deleteList, setChecked, handleEdit}){
 
     return (
             <form onSubmit={e => e.preventDefault()}>
-            <Grid
-            direction="row"
-            justifyContent="flex-start"
-            container
-            spacing={2}
-            >
-                <Grid item xs="auto">
-                    <Checkbox 
-                        size="small" 
-                        sx={{color: "blue", '&.Mui-checked': {color: "green"}}}
-                        onChange={() => setChecked(list.title)}
-                        checked={list.isCompleted}
-                    />
-                </Grid>
+                <Grid
+                direction="row"
+                justifyContent="flex-start"
+                alignItems="center"
+                container
+                spacing={2}
+                >
+                    <Grid item xs="auto">
+                        <Checkbox 
+                            size="small" 
+                            sx={{color: "blue", '&.Mui-checked': {color: "green"}}}
+                            onChange={() => setChecked(list.title)}
+                            checked={list.isCompleted}
+                        />
+                    </Grid>
 
-                <Grid item xs={3}>
-                    <h6 style={style}>{list.title}</h6>
-                </Grid>
+                    <Grid item xs={3} justifyContent="flex-end">
+                        <h6 style={style}>{list.title}</h6>
+                    </Grid>
 
-                {!list.isCompleted && 
-                    <Grid item xs={3}>
-                    <Button 
+                    {!list.isCompleted && 
+                        <Grid item xs="auto">
+                        <Button 
+                            size="small"
+                            color="primary"
+                            onClick={() => handleEdit(list)}
+                        > 
+                            <EditIcon fontSize="small"/>
+                        </Button>
+                        </Grid>
+                    }
+
+                    <Grid item xs="auto">
+                    <Button
+                        variant="text" 
+                        color="error"
                         size="small"
-                        color="primary"
-                        onClick={() => handleEdit(list)}
-                    > 
-                        <EditIcon fontSize="small"/>
+                        onClick={() => deleteList(list.title)}
+                    >
+                        <DeleteIcon fontSize="small" />
                     </Button>
                     </Grid>
-                }
-
-                <Grid item xs={3}>
-                <Button
-                    variant="text" 
-                    color="error"
-                    size="small"
-                    onClick={() => deleteList(list.title)}
-                >
-                    <DeleteIcon fontSize="small" />
-                </Button>
                 </Grid>
-            </Grid>
             </form>
     )
 }
